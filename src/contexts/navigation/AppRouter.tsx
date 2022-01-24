657e7c649bd41f67814e16c467c8a16579962d95
Michael Uzan <looply3@gmail.com>
2022-01-24 22:35:39 +0200

    add page not found

--- src/contexts/navigation/AppRouter.tsx
+++ src/contexts/navigation/AppRouter.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
 import HomePage from "./pages/HomePage";
 import CatalogPage from "./pages/CatalogPage";
-
 import pathsApp from "./pathsApp";
 import ProductPage from "./pages/ProductPage";
 import { AppHeader } from "../../tools/ui_components/AppHeader/AppHeader";
 import { CartPage } from "./pages/CartPage";
+import { PageNotFound } from "./pages/PageNotFound";
 
 const AppRouter = () => (
   <Router>
@@ -16,6 +15,7 @@ const AppRouter = () => (
       <Route path={pathsApp.product} component={ProductPage} />
       <Route path={pathsApp.catalog} component={CatalogPage} />
       <Route exact path={pathsApp.home} component={HomePage} />
+      <Route component={PageNotFound} />
     </Switch>
   </Router>
 );
---
+++ src/contexts/navigation/pages/PageNotFound.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const PageNotFound = () => {
+    return <StyledPageNotFound>
+        <h1>Page Not Found</h1>
+    </StyledPageNotFound>
+};
+
+const StyledPageNotFound = styled.div`
+    display:grid;
+    place-content: center;
+`
+
