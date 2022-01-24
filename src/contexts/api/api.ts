e137367e7f0edb10a1f25db1fc442f9c0fcb0c7a
Michael Uzan <looply3@gmail.com>
2022-01-24 23:19:17 +0200

    fixed bugs in deploy

--- src/contexts/api/api.ts
+++ src/contexts/api/api.ts
@@ -10,7 +10,7 @@ import { CatalogProduct } from "../../modules/product/types";
 const API = {
 	product: {
 		getAll: async (): Promise<CatalogProduct[]> =>
-			fetch("../db/products.json", {
+			fetch("db/products.json", {
 				headers: {
 					'Content-Type': 'application/json',
 					'Accept': 'application/json'
